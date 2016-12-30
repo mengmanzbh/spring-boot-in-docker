b2c1945055a434eb17aaaaca9a384722e69303f3
jihao <jihao@quancheng-ec.com>
2016-12-30 21:48:32 +0800

    add a simple spring boot app with only one api

---
+++ src/main/java/com/superhao/HomeController.java
@@ -0,0 +1,19 @@
+package com.superhao;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/hello", method = { RequestMethod.GET }, produces = { MediaType.APPLICATION_JSON_VALUE })
+	@ResponseBody
+	public String sayHello(@RequestParam(defaultValue = "Docker") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
---
+++ src/main/java/com/superhao/SpringBootInDockerApplication.java
@@ -0,0 +1,12 @@
+package com.superhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInDockerApplication.class, args);
+	}
+}
